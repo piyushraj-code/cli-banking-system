02062cfc38280459d6be0e85a5a290bec33acc48
Piyush Raj <raj4piyush@gmail.com>
2025-07-30 20:12:31 +0530

    added interface.py

---
+++ interface.py
@@ -0,0 +1,15 @@
+import time
+def commands():
+    print("Hello! Welcome to lena dena bank.")
+    time.sleep(0.2)
+    print("Enter 1 to create account")
+    time.sleep(0.2)
+    print("Enter 2 to check balance")
+    time.sleep(0.2)
+    print("Enter 3 to deposit")
+    time.sleep(0.2)
+    print("Enter 4 to withdraw")
+    time.sleep(0.2)
+    print("Enter 5 to exit")
+    time.sleep(0.2)
+  
